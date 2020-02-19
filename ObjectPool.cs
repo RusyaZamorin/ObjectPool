39c153289ad5d7cb7d06fc9fb2337914cb3f9605
RusyaZamorin <ruslanzamorin2000@gmail.com>
2020-02-19 20:09:09 +0200

    v1

---
+++ ObjectPool.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectPool : MonoBehaviour
+{
+    private List<GameObject> objects = new List<GameObject>();
+    private void Create(GameObject obj,Vector3 pos, Quaternion rot)
+    {
+        GameObject temp = Instantiate(obj,pos,rot);
+        objects.Add(temp);
+    }
+    public GameObject TakeObject(GameObject obj, Vector3 pos, Quaternion rot)
+    {
+        if (objects.Count == 0)
+        {
+            Create(obj, pos, rot);
+            return objects[0];
+        }
+        for (int i = 0; i < objects.Count; i++)
+        {
+            if (!objects[i].activeInHierarchy)
+            {
+                objects[i].transform.position = pos;
+                objects[i].transform.rotation = rot;
+                objects[i].SetActive(true);
+                return objects[i];
+            }
+        }
+        Create(obj, pos, rot);
+        return objects[objects.Count -1];
+    }
+
+}
